496585db11b33b74e33755c2ff7f10f5ca71866b
Carson Reinke <carson@reinke.co>
2020-07-13 11:15:07 -0400

    Add in initial Redux via RTK

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './app/App';
 import { HashRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <Provider>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('app')
 );
